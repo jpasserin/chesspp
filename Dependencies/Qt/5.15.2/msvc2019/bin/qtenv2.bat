961f45621309854c9e8f675681421355f3813a87
Jeremie Passerin <jpasserin@gmail.com>
2022-12-17 14:41:09 -0800

    Added Qt Library

---
+++ Dependencies/Qt/5.15.2/msvc2019/bin/qtenv2.bat
@@ -0,0 +1,5 @@
+@echo off
+echo Setting up environment for Qt usage...
+set PATH=D:\Jeremie\Code\References\TestProject\Dependencies\Qt\5.15.2\msvc2019\bin;%PATH%
+cd /D D:\Jeremie\Code\References\TestProject\Dependencies\Qt\5.15.2\msvc2019
+echo Remember to call vcvarsall.bat to complete environment setup!
